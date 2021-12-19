7ce33e8cd130d264cd6174280d18891143756eaa
Chrys Rakotonimanana <40733956+chrys-elrak@users.noreply.github.com>
2021-12-19 07:51:41 +0300

    The fiscal code

---
+++ The fiscal code/index.js
@@ -0,0 +1,13 @@
+const months = { 1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "H",
+7: "L", 8: "M", 9: "P", 10: "R", 11: "S", 12: "T" }
+const vowels = 'aeyuio'.split("");
+const consonants = 'qwrtpsdfghjklzxcvbnm'.split("");
+
+/**
+ * 
+ * @param {{ name: string,surname: string, gender: string, dob: string }} person 
+ */
+function fiscalCode(person) {
+
+	const nameChar = person.name.split("");
+}
